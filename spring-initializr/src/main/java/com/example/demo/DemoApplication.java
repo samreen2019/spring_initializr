9a2caaf4121ef1653c3aa588ed4bc3137852b488
samreen2019 <56146830+samreen2019@users.noreply.github.com>
2020-11-26 18:41:50 +0500

    made new spring initailzr project with java 15

--- spring-initializr/src/main/java/com/example/demo/DemoApplication.java
+++ spring-initializr/src/main/java/com/example/demo/DemoApplication.java
@@ -3,28 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-}
-
-@RestController
-class GreetingController {
-    
-    @RequestMapping("/hello/{name}")
-    String hello(@PathVariable String name) {
-        return "Hello, " + name + "!";
-    }
 }
